17cf5348b3c3b5ec67f796905c93f22ed92a28b8
Jean <jeancarlovg199@gmail.com>
2025-11-14 16:09:15 -0600

    se trabajo en la clase onstaculos( se configuro su velocidad y demas), se agrego el meteorito en principal y su metodo se incorporo en el bucle del juego, ademas se agregaron nuevos fondos

--- Proyecto-Calculo-II-STARFOX-64-main/src/entities/Obstaculos.py
+++ Proyecto-Calculo-II-STARFOX-64-main/src/entities/Obstaculos.py
@@ -1,12 +1,14 @@
 # en esta clase estaran todos los obstaculos del mapa
 
 import random
-
+import pygame
+from constantes import ALTO
+from entities.Objetos_Madre import ObjetoJuego
 
 class Obstaculos(ObjetoJuego):
 
     def __init__(self, pos_x, pos_y, velocidad, imagen):
-        super().__init__(pos_x, pos_y, velocidad, imagen)
+        super().__init__(pos_x, pos_y, vida_inicial=100, imagen=imagen, tam=(40, 40))
 
         # configuracion de caida de los objetos
 
@@ -14,13 +16,13 @@ class Obstaculos(ObjetoJuego):
         self.rect.y = random.randint(-100, -40)  # Aparece fuera de la pantalla en la parte superior
 
     
-    def update(self, dt):
-        # Mover el meteorito hacia abajo
-        self.rect.y = self.rect.y + self.velocidad_y  # Mueve el meteorito hacia abajo
+    def update(self, dt): #dt = delta time
+        
+        self.rect.y = self.rect.y + self.veloc_caida_y  # Mueve el meteorito hacia abajo
         
         # Si el meteorito toca el borde inferior, eliminarlo // logica factiable a cambiar
         if self.rect.top >= ALTO:
             self.kill()  # Elimina el meteorito cuando se sale de la pantalla    
 
-    # Metodo para crear los meteoriotos, pensado a implementarse con random para que vengan y se vayan solos
-    # def crear_meteoritos(): 
+ 
+            
--- Proyecto-Calculo-II-STARFOX-64-main/src/principal.py
+++ Proyecto-Calculo-II-STARFOX-64-main/src/principal.py
@@ -2,10 +2,13 @@
 
 #Imports necesarios para la conexión entre clases
 import pygame
+import random
 from constantes import ANCHO, ALTO, FPS, NEGRO
 from entities.airwing import Arwing
 from entities.Enemigos import Enemigos
+from entities.Obstaculos import Obstaculos
 from fondo import fondo
+
 import os
 
 # Iniciar Pygame
@@ -26,10 +29,32 @@ grupo_balas = pygame.sprite.Group()
 arwing = Arwing()
 todos_los_sprites.add(arwing)
 
+# Creacion de meteoritos
+meteoritos = pygame.sprite.Group() # unir los meteoritos al sprite de pygame
+
 # Creacion de nave enemiga
 ruta_imagen_enemigo = os.path.join("Proyecto-Calculo-II-STARFOX-64-main/assets/images/enemies/Enemigo_rojo_no_fondo.png")
 enemigo = Enemigos(ruta_imagen=ruta_imagen_enemigo, pos_x= 100, pos_y= 50, velocidad=5)  
 todos_los_sprites.add(enemigo)
+
+# Metodo para crear los meteoriotos, pensado a implementarse con random para que vengan y se vayan solos
+def crear_meteoritos():
+    if random.random() < 0.02:  # Probabilidad de aparición
+        # notas para Fabio 
+        # Aquí se creo un meteorito usando la clase Obstaculos
+        # pos_x: le puse en una posición aleatoria horizontal dentro de la pantalla.
+        # pos_y: le puse -40 para que aparezca arriba de la pantalla y caiga hacia abajo. ( calculo robado )
+        # velocidad: un numero aleatorio entre 2 y 5 para que algunos caigan más rápido y otros más lento. ( calculo robado )
+        # imagen: la imagen es solo de prueba, se puede usar una mejor si la tenemos
+
+        meteorito = Obstaculos(
+            pos_x=random.randint(0, ANCHO - 40),
+            pos_y=-40,
+            velocidad=random.randint(2, 5),
+            imagen="Proyecto-Calculo-II-STARFOX-64-main/assets/images/enemies/Asteroid_2_minerals.png"
+        )
+        meteoritos.add(meteorito)
+        todos_los_sprites.add(meteorito)
 
 
 # Clase borrador de proyectil
@@ -66,6 +91,10 @@ while ejecutando:
                 grupo_balas.add(nuevo_proyectil)
                 todos_los_sprites.add(nuevo_proyectil)
 
+
+    crear_meteoritos() # se llama la funcion para que este dentro del bucle del juego
+
+
     fondo_juego.actualizar(segundos_por_frame)
     todos_los_sprites.update(segundos_por_frame)
     arwing.mover(segundos_por_frame)
@@ -74,6 +103,7 @@ while ejecutando:
     for enemigo in todos_los_sprites:
         if isinstance(enemigo, Enemigos):  # Mover solo los enemigos # hay que modificar la condicion para que se mueva diferente
             enemigo.mover()
+    # LLamar los meteoritos
             
     fondo_juego.dibujar_en(ventana)
     todos_los_sprites.draw(ventana)
