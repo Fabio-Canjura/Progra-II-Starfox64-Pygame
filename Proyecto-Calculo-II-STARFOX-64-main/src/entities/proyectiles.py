1a24bf173dfe4d74d8b709992bdf8941135845e9
Fabio-Canjura <fcanjuram@edu.uia.ac.cr>
2025-11-16 15:04:12 -0600

    Modified proyectiles class to load disparo type from airwing class.

--- Proyecto-Calculo-II-STARFOX-64-main/src/entities/proyectiles.py
+++ Proyecto-Calculo-II-STARFOX-64-main/src/entities/proyectiles.py
@@ -4,20 +4,24 @@ from constantes import ALTO
 
 class Proyectil(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, velocidad_y=-600, danio=10):
+    def __init__(self, x, y, velocidad_y=-600, danio=10, color=None, ancho=None, alto=None):
         super().__init__()
-        ruta_proyectil = os.path.join("assets", "images", "player", "Airwing_bullet.png")
-
-        try:
-            self.image = pygame.image.load(ruta_proyectil).convert_alpha()
-            self.image = pygame.transform.scale(self.image, (12, 24))
-        except Exception:
-            # En caso de error usa un placeholder
-            self.image = pygame.Surface((5, 15))
-            self.image.fill((0, 200, 255))
-        # posición inicial de nave
+        #Carga de proyectiles en pantalla desde diccionario
+        if color is not None and ancho is not None and alto is not None:
+            # Proyectil generado desde el diccionario
+            self.image = pygame.Surface((ancho, alto))
+            self.image.fill(color)
+        else:
+            # Proyectil por defecto usando imagen
+            ruta_proyectil = os.path.join("assets", "images", "player", "Airwing_bullet.png")
+            try:
+                self.image = pygame.image.load(ruta_proyectil).convert_alpha()
+                self.image = pygame.transform.scale(self.image, (12, 24))
+            except Exception:
+                self.image = pygame.Surface((5, 15))
+                self.image.fill((0, 200, 255))
+                
         self.rect = self.image.get_rect(center=(x, y))
-        # Atributos propios de los proyectiles.
         self.velocidad_y = velocidad_y
         self.danio = danio
 
