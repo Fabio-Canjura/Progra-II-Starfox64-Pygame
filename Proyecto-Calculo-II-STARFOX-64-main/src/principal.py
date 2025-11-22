4e1759f7503f760c874629e593f498754762e728
Fabio-Canjura <fcanjuram@edu.uia.ac.cr>
2025-11-22 12:02:24 -0600

    General correction and moved generation of meteoritos to own class.

--- Proyecto-Calculo-II-STARFOX-64-main/src/principal.py
+++ Proyecto-Calculo-II-STARFOX-64-main/src/principal.py
@@ -9,6 +9,8 @@ from entities.Enemigos import Enemigos
 from entities.Obstaculos import Obstaculos
 from fondo import fondo
 from entities.power_up import power_up
+from entities.meteoritos import OrquestrarMeteoritos
+
 
 import os
 
@@ -31,7 +33,8 @@ arwing = Arwing()
 todos_los_sprites.add(arwing)
 
 # Creacion de meteoritos
-meteoritos = pygame.sprite.Group() # unir los meteoritos al sprite de pygame
+gestor_meteoritos = OrquestrarMeteoritos() # unir los meteoritos al sprite de pygame
+meteoritos = pygame.sprite.Group()
 
 #Creación de powerups
 grupo_powerups = pygame.sprite.Group()
@@ -41,22 +44,7 @@ ruta_imagen_enemigo = os.path.join("assets", "images", "enemies", "Enemigo_rojo_
 enemigo = Enemigos(ruta_imagen=ruta_imagen_enemigo, pos_x= 100, pos_y= 50, velocidad=5)  
 todos_los_sprites.add(enemigo)
 
-# Metodo para crear los meteoriotos, pensado a implementarse con random para que vengan y se vayan solos
-def crear_meteoritos():
-    if random.random() < 0.02:  # Probabilidad de aparición
-        # notas para Fabio 
-        # Aquí se creo un meteorito usando la clase Obstaculos
-        # pos_x: le puse en una posición aleatoria horizontal dentro de la pantalla.
-        # pos_y: le puse -40 para que aparezca arriba de la pantalla y caiga hacia abajo. ( calculo robado )
-        # velocidad: un numero aleatorio entre 2 y 5 para que algunos caigan más rápido y otros más lento. ( calculo robado )
-        # imagen: la imagen es solo de prueba, se puede usar una mejor si la tenemos
-
-        ruta_imagen_meteorito = os.path.join("assets", "images", "enemies", "Asteroid_2_minerals.png")
-        meteorito = Obstaculos(pos_x=random.randint(0, ANCHO - 40), pos_y=-40, velocidad=random.randint(2, 5),ruta_imagen=ruta_imagen_meteorito)
-        meteoritos.add(meteorito)
-        todos_los_sprites.add(meteorito)
-
-# Meotodo para detectar la colision de la nave
+# Metodo para detectar la colision de la nave
 def detectar_colision_nave_meteoritos(nave, grupo_meteoritos):
     colisiones = pygame.sprite.spritecollide(nave, grupo_meteoritos, False)
     if colisiones:
@@ -93,61 +81,48 @@ def dibujar_barra_vida(pantalla, x, y, vida, vida_maxima):
 # Bucle principal de la pantalla.
 ejecutando = True
 while ejecutando:
-    segundos_por_frame=clock.tick(FPS) / 1000.0
-    #Bucle que obtiene interacción con ventana.
+    segundos_por_frame = clock.tick(FPS) / 1000
+    # Eventos del juego
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             ejecutando = False
+
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 resultado = arwing.disparar(grupo_balas)
-                #Momentaneo para verificar si se cuentan los disparos 
-                print("🔵 DISPAROS REGISTRADOS:", arwing.estadisticas.get("disparos", 0))
                 for bala in grupo_balas:
                     if bala not in todos_los_sprites:
                         todos_los_sprites.add(bala)
-                
-                # Verificar si se debe generar powerup
+
+                # PowerUp
                 if arwing.estadisticas.get("disparos", 0) >= 25:
-                    print("💫 Se generó un PowerUp por 25 disparos!")
-                    arwing.estadisticas["disparos"] = 0  # Reiniciar contador
+                    print("💫 PowerUp generado")
+                    arwing.estadisticas["disparos"] = 0
                     crear_powerup()
 
+    #Creación de meteoritos
+    nuevo_meteorito = gestor_meteoritos.intentar_generar()
+    if nuevo_meteorito:
+        meteoritos.add(nuevo_meteorito)
+        todos_los_sprites.add(nuevo_meteorito)
 
-    crear_meteoritos() # se llama la funcion para que este dentro del bucle del juego
-
-     # Actualizamos la nave del jugador. Le pasamos el tiempo del frame y los meteoritos para que pueda detectar choques.
+    # Actualización general de los elementos en pantalla
     arwing.update(segundos_por_frame, meteoritos)
-
-    # Actualizamos TODOS los meteoritos. Solo necesitan saber cuánto duró el frame para moverse.
     meteoritos.update(segundos_por_frame)
-
-    # Actualizamos las balas. Estas solo se mueven hacia arriba, no ocupan el tiempo del frame. Da error si no se colocan xD
     grupo_balas.update(segundos_por_frame)
-
-    # Actualizamos al enemigo. También usa el tiempo del frame para moverse de forma más suave.
     enemigo.update(segundos_por_frame)
-    
-    # deteccion de colisiones
+    grupo_powerups.update(segundos_por_frame)
+
+    # Colisiones de meteoritos con nave
     if detectar_colision_nave_meteoritos(arwing, meteoritos):
         arwing.aplicar_lentitud()
 
-
+    # Carga de sprites en la pantalla de juego
     fondo_juego.actualizar(segundos_por_frame)
-    
-    """
-    # mover los enemigos
-    for enemigo in todos_los_sprites:
-        if isinstance(enemigo, Enemigos):  # Mover solo los enemigos # hay que modificar la condicion para que se mueva diferente
-            enemigo.mover()
-    # LLamar los meteoritos
-    """
-            
     fondo_juego.dibujar_en(ventana)
-    todos_los_sprites.draw(ventana)
 
-    # Dibujar la barra de vida en la esquina inferior derecha
-    dibujar_barra_vida(ventana,ANCHO - 70,ALTO - 30,arwing.salud,100)
+    todos_los_sprites.draw(ventana)
+    dibujar_barra_vida(ventana, ANCHO - 70, ALTO - 30, arwing.salud, 100)
 
     pygame.display.flip()
 
