df9cc7dc1f2932d7578bb0c93c45d30b3465961d
Fabio-Canjura <fcanjuram@edu.uia.ac.cr>
2025-11-22 23:40:27 -0600

    General corrections.

--- Proyecto-Calculo-II-STARFOX-64-main/src/decorador.py
+++ Proyecto-Calculo-II-STARFOX-64-main/src/decorador.py
@@ -1,10 +1,10 @@
 
-#Decorador que incrementa un contador de eventos dentro del Arwing.
+#Decorador que incrementa un contador de eventos dentro del Arwing para generar el powerup.
 def registrar_evento(nombre_evento):
-    def decorador(funcion_original):
+    def decorador(funcion):
         def funcion_decorada(self, grupo_balas):
             # Ejecutar la función
-            se_disparo = funcion_original(self, grupo_balas)
+            se_disparo = funcion(self, grupo_balas)
             # Solo contar si REALMENTE disparó
             if se_disparo:
                 self.estadisticas[nombre_evento] = self.estadisticas.get(nombre_evento, 0) + 1
